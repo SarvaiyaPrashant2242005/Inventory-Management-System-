f0a7c8110d95a61cb1d6ae0bb1e6369da25109e2
SarvaiyaPrashant2242005 <115221462+SarvaiyaPrashant2242005@users.noreply.github.com>
2024-03-23 15:35:30 +0530

    ADMIN side

--- Admin_Homepage.php
+++ Admin_Homepage.php
@@ -1,4 +1,10 @@
 <h1>
             This is Admin Home Page
         </h1>
-<a href="Add_Components.html">Add Components</a>
+    <a href='Add_Microcontrollar.html'>Microcontrollars</a>
+    <br>
+    <a href='Add_Sensors.html'>Sensors</a>
+    <br>
+    <a href='Add_Drone_components.html'>Drone Components</a>
+    <br>
+    <a href='Add_Electronic_items.html'>Electronics Items</a>
